34d0fbfd0e31ed8d2259f56b970dd74156a4af74
LAKKI <thakurluckysinghbrh@gmail.com>
2021-03-10 01:46:59 +0530

    .

---
+++ digitsum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    scanf("%d", &x);
+   int sumdigits(int inp)
+    {
+        int sum = 0;
+        while (inp > 0)
+        {
+
+            sum += inp % 10;
+            inp = inp / 10;
+           
+        }
+        if(sum>10)
+        sumdigits(sum);
+         else return sum;
+    }
+    printf("The Single integer sum of given number is  %d\n", sumdigits(x));
+    return 0;
+}
